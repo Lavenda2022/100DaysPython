91a4535048f917be83b93560b54c61d277765f93
Lavenda <103087649+Lavenda2022@users.noreply.github.com>
2022-12-02 10:00:26 +1300

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,17 @@
+#Rock Paper Scissors
+
+import random
+
+you = int(input("What do you choose? Type 0 for rock,1 for paper or 2 for scissors.\n"))
+
+computer = random.randint(0,2)
+print(f"Computer choose {computer}.")
+
+if (you == 0) and (computer == 2):
+    print("You win.")
+elif computer > you:
+    print("You lose.")
+elif computer == you:
+    print("It is a draw.")
+else:
+    print("Invalid input, you lose!")
